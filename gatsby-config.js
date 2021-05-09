f2e462774f191797956ffe25b4e0642be3ade052
Thomas Lamars <thomas@lamars.nl>
2021-05-09 16:01:45 +0200

    Added initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+require("dotenv").config({
+    path: `.env`,
+})
+
+module.exports = {
+    siteMetadata: {
+        title: "Portfolio",
+    },
+    plugins: [
+        {
+            resolve: "gatsby-source-contentful",
+            options: {
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+            },
+        },
+        "gatsby-plugin-postcss",
+        "gatsby-plugin-sass",
+        "gatsby-plugin-gatsby-cloud",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+    ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import * as React from "react"
+
+export default function Home() {
+    
+    return (
+        <h1>Dit is de portfolio van Thomas</h1>
+    )
+}
